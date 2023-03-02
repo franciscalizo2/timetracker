d58678d905f3861f8812c046957ebbccf240d1fb
francis <francis@aahcs.com>
2023-03-02 18:13:54 -0500

    feat: implement Add Timesheet

--- src/components/layout/dashboard/Timesheets/AddTimesheet/index.tsx
+++ src/components/layout/dashboard/Timesheets/AddTimesheet/index.tsx
@@ -3,15 +3,18 @@ import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 
 import BackToLink from 'src/components/layout/dashboard/BackToLink';
-
+import { useTimesheets } from 'src/context/timesheetsContext';
 import classes from './AddTimesheet.module.css';
 
-type FormValues = {
-  referenceNumber: string;
-  jobTitle: string;
-  firstName: string;
-  lastName: string;
+export type FormValues = {
   clientName: string;
+  consultantFirstName: string;
+  consultantLastName: string;
+  weekStarting: Date;
+  weekEnding: Date;
+  rate: number;
+  totalHours: number;
+  status: string;
 };
 
 export default function AddClient() {
@@ -22,18 +25,224 @@ export default function AddClient() {
   } = useForm<FormValues>();
 
   const navigate = useNavigate();
+  const { setTimesheetsList } = useTimesheets();
 
   const onSubmit = (data: FormValues) => {
-    console.log(data);
+    const {
+      clientName,
+      consultantFirstName,
+      consultantLastName,
+      weekStarting,
+      weekEnding,
+      rate,
+      totalHours,
+      status,
+    } = data;
+
+    const newClient = {
+      client: clientName,
+      firstName: consultantFirstName,
+      lastName: consultantLastName,
+      weekStarting,
+      weekEnding,
+      rate,
+      totalHours,
+      status,
+    };
+
+    setTimesheetsList((old: any) => [...old, newClient]);
+    navigate(`/dashboard/timesheets`);
   };
 
   return (
     <div>
       <div style={{ maxWidth: 200 }}>
-        <BackToLink to="/dashboard/timesheets" text="Back to Timesheets List" />
+        <BackToLink to="/dashboard/timesheets" text="Back to Client List" />
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <h2>Coming Soon</h2>
+        <div className={classes['page-container']}>
+          <div className={classes['form-container']}>
+            <h2>Add A Timesheet</h2>
+
+            <div className={classes['grid-container']}>
+              <div className={`${classes['client-name']}`}>
+                <label className={classes['input-label']} htmlFor="clientName">
+                  Client Name
+                </label>
+                <input
+                  id="clientName"
+                  className={classes['input']}
+                  type="text"
+                  {...register('clientName', {
+                    required: 'This field is required',
+                  })}
+                />
+                {errors.clientName && (
+                  <div className={classes['error-message']}>
+                    {errors.clientName.message}
+                  </div>
+                )}
+              </div>
+
+              <div className={`${classes['consultant-first-name']}`}>
+                <label
+                  className={classes['input-label']}
+                  htmlFor="consultantFirstName"
+                >
+                  Consultant First Name
+                </label>
+                <input
+                  id="consultantFirstName"
+                  className={classes['input']}
+                  type="text"
+                  {...register('consultantFirstName', {
+                    required: 'This field is required',
+                  })}
+                />
+                {errors.consultantFirstName && (
+                  <div className={classes['error-message']}>
+                    {errors.consultantFirstName.message}
+                  </div>
+                )}
+              </div>
+
+              <div className={`${classes['consultant-last-name']}`}>
+                <label
+                  className={classes['input-label']}
+                  htmlFor="consultantLastName"
+                >
+                  Consultant Last Name
+                </label>
+                <input
+                  id="consultantLastName"
+                  className={classes['input']}
+                  type="text"
+                  {...register('consultantLastName', {
+                    required: 'This field is required',
+                  })}
+                />
+                {errors.consultantLastName && (
+                  <div className={classes['error-message']}>
+                    {errors.consultantLastName.message}
+                  </div>
+                )}
+              </div>
+
+              <div className={`${classes['week-starting']}`}>
+                <label
+                  className={classes['input-label']}
+                  htmlFor="weekStarting"
+                >
+                  Week Starting
+                </label>
+                <input
+                  id="weekStarting"
+                  className={classes['input']}
+                  type="date"
+                  {...register('weekStarting', {
+                    required: 'This field is required',
+                  })}
+                />
+                {errors.weekStarting && (
+                  <div className={classes['error-message']}>
+                    {errors.weekStarting.message}
+                  </div>
+                )}
+              </div>
+
+              <div className={`${classes['week-ending']}`}>
+                <label className={classes['input-label']} htmlFor="weekEnding">
+                  Week Ending
+                </label>
+                <input
+                  id="weekEnding"
+                  className={classes['input']}
+                  type="date"
+                  {...register('weekEnding', {
+                    required: 'This field is required',
+                  })}
+                />
+                {errors.weekEnding && (
+                  <div className={classes['error-message']}>
+                    {errors.weekEnding.message}
+                  </div>
+                )}
+              </div>
+
+              <div className={`${classes['rate']}`}>
+                <label className={classes['input-label']} htmlFor="rate">
+                  Rate
+                </label>
+                <input
+                  id="rate"
+                  className={classes['input']}
+                  type="number"
+                  {...register('rate', {
+                    required: 'This field is required',
+                  })}
+                />
+                {errors.rate && (
+                  <div className={classes['error-message']}>
+                    {errors.rate.message}
+                  </div>
+                )}
+              </div>
+
+              <div className={`${classes['total-hours']}`}>
+                <label className={classes['input-label']} htmlFor="totalHours">
+                  Total Hours
+                </label>
+                <input
+                  id="totalHours"
+                  className={classes['input']}
+                  type="number"
+                  {...register('totalHours', {
+                    required: 'This field is required',
+                  })}
+                />
+                {errors.totalHours && (
+                  <div className={classes['error-message']}>
+                    {errors.totalHours.message}
+                  </div>
+                )}
+              </div>
+
+              <div className={`${classes['status']}`}>
+                <label className={classes['input-label']} htmlFor="status">
+                  Status
+                </label>
+                <input
+                  id="status"
+                  className={classes['input']}
+                  type="text"
+                  {...register('status', {
+                    required: 'This field is required',
+                  })}
+                />
+                {errors.status && (
+                  <div className={classes['error-message']}>
+                    {errors.status.message}
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <div className={classes['buttons-container']}>
+              <Link to={'/dashboard/timesheets'}>
+                <button className={`${classes['button']} ${classes['cancel']}`}>
+                  Cancel
+                </button>
+              </Link>
+
+              <button
+                type="submit"
+                className={`${classes['button']} ${classes['add']}`}
+              >
+                Add
+              </button>
+            </div>
+          </div>
+        </div>
       </form>
     </div>
   );
